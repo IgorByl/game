d7f724cf67ec5b34beca9207406f4c4b291bef06
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-08 19:51:12 +0000

    Deploy IgorByl/game to github.com/IgorByl/game.git:gh-pages

---
+++ precache-manifest.9c2898797008824ad7b74df15ca1600d.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d1dd2ab6bdd89741693",
+    "url": "/static/js/0.5d1dd2ab.chunk.js"
+  },
+  {
+    "revision": "5a7648f96e500e6055f6",
+    "url": "/static/css/main.736a83a2.chunk.css"
+  },
+  {
+    "revision": "5a7648f96e500e6055f6",
+    "url": "/static/js/main.5a7648f9.chunk.js"
+  },
+  {
+    "revision": "290bf73af3cbdcafedb1",
+    "url": "/static/css/2.efc6ecde.chunk.css"
+  },
+  {
+    "revision": "290bf73af3cbdcafedb1",
+    "url": "/static/js/2.290bf73a.chunk.js"
+  },
+  {
+    "revision": "6ecaaf0c13c30920769d",
+    "url": "/static/css/3.382b17da.chunk.css"
+  },
+  {
+    "revision": "6ecaaf0c13c30920769d",
+    "url": "/static/js/3.6ecaaf0c.chunk.js"
+  },
+  {
+    "revision": "4eb7499a6c4d2f58238f",
+    "url": "/static/css/4.f6cc847b.chunk.css"
+  },
+  {
+    "revision": "4eb7499a6c4d2f58238f",
+    "url": "/static/js/4.4eb7499a.chunk.js"
+  },
+  {
+    "revision": "d4c1ead60e9b3adba4ef",
+    "url": "/static/js/5.d4c1ead6.chunk.js"
+  },
+  {
+    "revision": "46351c7b060ceb1b4e3b",
+    "url": "/static/js/6.46351c7b.chunk.js"
+  },
+  {
+    "revision": "1d762730c2731b63ee90",
+    "url": "/static/js/runtime~main.1d762730.js"
+  },
+  {
+    "revision": "55341a5d477140dffe9eca7cdac4ed93",
+    "url": "/static/media/modalWindowBorder.55341a5d.png"
+  },
+  {
+    "revision": "8544fb3344ae50211bd64207bff98558",
+    "url": "/static/media/title.8544fb33.jpg"
+  },
+  {
+    "revision": "99f40e318f9493499e6f89e3f2ead1af",
+    "url": "/static/media/gameLogo.99f40e31.png"
+  },
+  {
+    "revision": "efed08aec6e5e2b424bb3de2e0f14ac3",
+    "url": "/static/media/orgrimmar.efed08ae.jpg"
+  },
+  {
+    "revision": "758ae711c854ffa8d393807a207abf61",
+    "url": "/static/media/buttonBorder.758ae711.png"
+  },
+  {
+    "revision": "b2f6c8209124039c250b8bb4780fbfe6",
+    "url": "/static/media/fireball.b2f6c820.png"
+  },
+  {
+    "revision": "ef7cd0a8f26fd3aca510a4b198cdda66",
+    "url": "/static/media/sword.ef7cd0a8.png"
+  },
+  {
+    "revision": "b408e291d86e3313e7252ea28efff76c",
+    "url": "/static/media/heal.b408e291.png"
+  },
+  {
+    "revision": "9468b8b08c6608f1a5969884a153a44f",
+    "url": "/static/media/fireball.9468b8b0.mp3"
+  },
+  {
+    "revision": "8403cc1f56b42e20cd6f717d963e29cc",
+    "url": "/static/media/heal.8403cc1f.mp3"
+  },
+  {
+    "revision": "ea4652037118717bab0687189e2d8cae",
+    "url": "/static/media/automobile.ea465203.mp3"
+  },
+  {
+    "revision": "1515ebedf1c428582636c7ee16c31a9a",
+    "url": "/static/media/boot.1515ebed.mp3"
+  },
+  {
+    "revision": "37d018190deb5db5e7e484f2bfaa5d67",
+    "url": "/static/media/breakfast.37d01819.mp3"
+  },
+  {
+    "revision": "ccfd0e733f28a21bf24023e8a8e8dfcf",
+    "url": "/static/media/button.ccfd0e73.mp3"
+  },
+  {
+    "revision": "dd08243ef5afb8dac9689ec47f091dbf",
+    "url": "/static/media/candle.dd08243e.mp3"
+  },
+  {
+    "revision": "2ff221e48d8ff70dcd08086ce18b8b10",
+    "url": "/static/media/chest.2ff221e4.mp3"
+  },
+  {
+    "revision": "75f5aa2c608469cf722777aa41a906ae",
+    "url": "/static/media/chicken.75f5aa2c.mp3"
+  },
+  {
+    "revision": "3379f2c556713f961ff01f4130445a48",
+    "url": "/static/media/childhood.3379f2c5.mp3"
+  },
+  {
+    "revision": "5d5a95a5be271cd2ca85542b08db2fd8",
+    "url": "/static/media/doctor.5d5a95a5.mp3"
+  },
+  {
+    "revision": "15f1b500d07939d693468806aa477fc3",
+    "url": "/static/media/game.15f1b500.mp3"
+  },
+  {
+    "revision": "ddfdfc65eefee252f4077b8673cb133c",
+    "url": "/static/media/jacket.ddfdfc65.mp3"
+  },
+  {
+    "revision": "770cb7c9711b772bf945aba89d8407d1",
+    "url": "/static/media/morning.770cb7c9.mp3"
+  },
+  {
+    "revision": "27f416fbe83e1b9cb2825ff3e96a772a",
+    "url": "/static/media/mother.27f416fb.mp3"
+  },
+  {
+    "revision": "b89e5be0cb1dd3b48ada522fb02018f1",
+    "url": "/static/media/night.b89e5be0.mp3"
+  },
+  {
+    "revision": "57c0214e59deba02f1dde92df977a6de",
+    "url": "/static/media/sound.57c0214e.mp3"
+  },
+  {
+    "revision": "f1bfe2f534ce031f2a5964000ca8fecb",
+    "url": "/static/media/stone.f1bfe2f5.mp3"
+  },
+  {
+    "revision": "e89f84d868b26e1730b4b7071700b836",
+    "url": "/static/media/ticket.e89f84d8.mp3"
+  },
+  {
+    "revision": "e451789559a6427ba116806ad96dfbc4",
+    "url": "/static/media/vocabulary.e4517895.mp3"
+  },
+  {
+    "revision": "6b4f86b6c232d94cba2da10ffb21de59",
+    "url": "/static/media/home.6b4f86b6.mp3"
+  },
+  {
+    "revision": "983b8d454026546acab6145618d0d41e",
+    "url": "/static/media/book.983b8d45.mp3"
+  },
+  {
+    "revision": "49e877a1f67d21335d0c453f0d12ca2e",
+    "url": "/static/media/cat.49e877a1.mp3"
+  },
+  {
+    "revision": "ab7bc752fad0fe324f0765d0bf6f9db0",
+    "url": "/static/media/dog.ab7bc752.mp3"
+  },
+  {
+    "revision": "97eee9c8bcab4539dff458ec072bfba9",
+    "url": "/static/media/door.97eee9c8.mp3"
+  },
+  {
+    "revision": "920e20bb0a4014854b7988603447b11e",
+    "url": "/static/media/key.920e20bb.mp3"
+  },
+  {
+    "revision": "a3908ced7ec7ef63a5d1773349cd7e4f",
+    "url": "/static/media/king.a3908ced.mp3"
+  },
+  {
+    "revision": "701f186332bf2a6b6398193a74554966",
+    "url": "/static/media/list.701f1863.mp3"
+  },
+  {
+    "revision": "e85ddccd6ba37943fd236c71f161c3d4",
+    "url": "/static/media/moon.e85ddccd.mp3"
+  },
+  {
+    "revision": "ab0c0be1bfe9a37407f1805416e35f47",
+    "url": "/static/media/note.ab0c0be1.mp3"
+  },
+  {
+    "revision": "0ac1359daa648126eb831f36ff8cced1",
+    "url": "/static/media/pen.0ac1359d.mp3"
+  },
+  {
+    "revision": "6ec01d52a0c8b3f3f94c0fb228267a37",
+    "url": "/static/media/red.6ec01d52.mp3"
+  },
+  {
+    "revision": "f63218798b792076e021cc28e1e60513",
+    "url": "/static/media/road.f6321879.mp3"
+  },
+  {
+    "revision": "e98de9a8f594c2dd1818742f5053c6e9",
+    "url": "/static/media/snow.e98de9a8.mp3"
+  },
+  {
+    "revision": "2adc51a04ced860b360a929f7ef96b4c",
+    "url": "/static/media/star.2adc51a0.mp3"
+  },
+  {
+    "revision": "fc9b522a873226eaeffde7665a5ca3d1",
+    "url": "/static/media/tea.fc9b522a.mp3"
+  },
+  {
+    "revision": "53e7e3053a1ddff5565e5f9892bb0f9d",
+    "url": "/static/media/tree.53e7e305.mp3"
+  },
+  {
+    "revision": "96f183819e7c4259629f80f9e4fdb547",
+    "url": "/static/media/lion.96f18381.mp3"
+  },
+  {
+    "revision": "0da2575290fe7619e5c51c9ac1160cfb",
+    "url": "/static/media/ballonBackground.0da25752.jpg"
+  },
+  {
+    "revision": "2459de2b6f31fb05036e78dbd6274716",
+    "url": "/static/media/body.2459de2b.png"
+  },
+  {
+    "revision": "3fb06ef94c81678449b5d7ea2682d246",
+    "url": "/static/media/head.3fb06ef9.png"
+  },
+  {
+    "revision": "0b59bfd33d079ef4b2a08485b3c1f5e5",
+    "url": "/static/media/lefthand.0b59bfd3.png"
+  },
+  {
+    "revision": "5ee1bf4386261e228305f01ec9832347",
+    "url": "/static/media/righthand.5ee1bf43.png"
+  },
+  {
+    "revision": "b6dcd628addab78f615a503bbb4633af",
+    "url": "/static/media/body.b6dcd628.png"
+  },
+  {
+    "revision": "2d262455673ce35144860dbe756d5def",
+    "url": "/static/media/head.2d262455.png"
+  },
+  {
+    "revision": "e6a2f21aa39786479c59a27a844fc5ab",
+    "url": "/static/media/lefthand.e6a2f21a.png"
+  },
+  {
+    "revision": "6a98c0485f977f1d4529ff709e98b171",
+    "url": "/static/media/righthand.6a98c048.png"
+  },
+  {
+    "revision": "c7607fe8f8003f0b40d589da06407c36",
+    "url": "/static/media/body.c7607fe8.png"
+  },
+  {
+    "revision": "20591fbf0c88477fc0dbf187e3f698b6",
+    "url": "/static/media/head.20591fbf.png"
+  },
+  {
+    "revision": "394a3f6334762d867c6fba85916dee19",
+    "url": "/static/media/lefthand.394a3f63.png"
+  },
+  {
+    "revision": "439a1a6505d84bf43dcd0c453e878fe4",
+    "url": "/static/media/righthand.439a1a65.png"
+  },
+  {
+    "revision": "e9928d2efe8d587a615763e488ae79db",
+    "url": "/static/media/levelUp.e9928d2e.mp3"
+  },
+  {
+    "revision": "edf2bc107737e7f927b88a4137a82b88",
+    "url": "/static/media/portal.edf2bc10.png"
+  },
+  {
+    "revision": "d4229b5deadbce3a4552d52062cae7d4",
+    "url": "/index.html"
+  }
+];
